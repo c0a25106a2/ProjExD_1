cd4db1b46a896f2174d91bd4278c3b237a96d96a
c0a25106a2 <c0a25106a2@edu.teu.ac.jp>
2026-04-14 16:42:05 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -21,16 +21,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        x = tmr % 3200 #練習5
+        kk_rct.move_ip(-1, 0)
         key_lst = pg.key.get_pressed() # 練習10 すべてのキーの押下状態取得
         if key_lst[pg.K_UP]: # 練習10 移動処理
             kk_rct.move_ip(0, -1)
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, 1)
+            kk_rct.move_ip(0, +1)
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1, 0)
-        x = tmr % 3200 #練習5
+            kk_rct.move_ip(+2, 0)
         screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(bg_img2, [-x+1600, 0]) #練習7
         screen.blit(bg_img, [-x+3200, 0]) #練習9
