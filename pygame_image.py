f3aa534815732c42a78721d68baaf81fde153f44
c0a25106a2 <c0a25106a2@edu.teu.ac.jp>
2026-04-14 15:54:25 +0900

    練習5と6

--- pygame_image.py
+++ pygame_image.py
@@ -13,15 +13,17 @@ def main():
     kk_img = pg.image.load("fig/3.png") # 練習3 こうかとん画像読み込み
     kk_img = pg.transform.flip(kk_img, True, False) #練習4 画像反転
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #練習2
+        x = tmr #練習5
+        screen.blit(bg_img, [-x, 0]) #練習2
         screen.blit(kk_img, [300, 200]) #練習3 こうかとん画像表示
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200) # 練習6
 
 
 if __name__ == "__main__":
